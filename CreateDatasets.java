57149cad875fbe0eec6fbc72c7d0b9eaa6be2ae8
DSkarlatou <skarlatou.danae@gmail.com>
2025-05-02 12:52:04 +0300

    update

--- CreateDatasets.java
+++ CreateDatasets.java
@@ -1,44 +1,43 @@
-import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.math.*;
 
 public class CreateDatasets {
-	private static   int min = -1;
-	private static   int max = 1;
+	private static final int min = -1;
+	private static final int max = 1;
 
-	public static float generate()
-	{
-		return (float) (min + Math.random() * (max - min));
+	public CreateDatasets() throws IOException {
+		writeData();
 	}
 
-	public static void main(String[] args) throws IOException {
-		
-		FileWriter trainingDataset = new FileWriter("training.txt");	
-		FileWriter testingDataset = new FileWriter("testing.txt");	
-	    		
+	public static void writeData() throws IOException {
+		FileWriter trainingDataset = new FileWriter("training.txt");
+		FileWriter testingDataset = new FileWriter("testing.txt");
+
 		for(int i = 0; i < 8000; i++)
 		{
 			float x1 = generate();
 			float x2 = generate();
 			int classification;
-			
-			if((x1-0.5)*(x1-0.5) + (x2-0.5)*(x2-0.5)<0.2 && x2>0.5)
+
+			boolean b1 = (x1 - 0.5) * (x1 - 0.5) + (x2 - 0.5) * (x2 - 0.5) < 0.2;
+			boolean b2 = (x1 + 0.5) * (x1 + 0.5) + (x2 + 0.5) * (x2 + 0.5) < 0.2;
+			boolean b3 = (x1 - 0.5) * (x1 - 0.5) + (x2 + 0.5) * (x2 + 0.5) < 0.2;
+			boolean b4 = (x1 + 0.5) * (x1 + 0.5) + (x2 - 0.5) * (x2 - 0.5) < 0.2;
+			if(b1 && x2>0.5)
 				classification = 1;
-			else if((x1-0.5)*(x1-0.5) + (x2-0.5)*(x2-0.5)<0.2 && x2<0.5)
+			else if(b1 && x2<0.5)
 				classification = 2;
-			else if((x1+0.5)*(x1+0.5) + (x2+0.5)*(x2+0.5)<0.2 && x2>-0.5)
+			else if(b2 && x2>-0.5)
 				classification = 1;
-			else if((x1+0.5)*(x1+0.5) + (x2+0.5)*(x2+0.5)<0.2 && x2<-0.5)
-				classification = 2;	
-			else if((x1-0.5)*(x1-0.5) + (x2+0.5)*(x2+0.5)<0.2 && x2>-0.5)
+			else if(b2 && x2<-0.5)
+				classification = 2;
+			else if(b3 && x2>-0.5)
 				classification = 1;
-			else if((x1-0.5)*(x1-0.5) + (x2+0.5)*(x2+0.5)<0.2 && x2<-0.5)
+			else if(b3 && x2<-0.5)
 				classification = 2;
-			else if((x1+0.5)*(x1+0.5) + (x2-0.5)*(x2-0.5)<0.2 && x2>0.5)
+			else if(b4 && x2>0.5)
 				classification = 2;
-			else if((x1+0.5)*(x1+0.5) + (x2-0.5)*(x2-0.5)<0.2 && x2<0.5)
+			else if(b4 && x2<0.5)
 				classification = 1;
 			else
 				classification = 3;
@@ -47,10 +46,15 @@ public class CreateDatasets {
 				trainingDataset.write(x1 +","+x2+","+classification+"\n");
 			else
 				testingDataset.write(x1 +","+x2+","+classification+"\n");
-			
+
 		}
 		trainingDataset.close();
 		testingDataset.close();
+	}
+
+	public static float generate()
+	{
+		return (float) (min + Math.random() * (max - min));
 	}
 
 }
